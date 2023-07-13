8e389d2ca35bc077c3f077b1b937a7647eed62c4
yashwanthreddy <yashwanth.kotha@onpassive.com>
2023-07-13 16:54:41 +0530

    code changes

---
+++ seed.groovy
@@ -0,0 +1,25 @@
+folder('CI-Pipelines') {
+  displayName('CI-Pipelines')
+  description('CI-Pipelines')
+}
+
+pipelineJob('CI-Pipelines/Jenkins') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/yashwanthk147/jenkins-slave.git')
+          }
+       }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+}
