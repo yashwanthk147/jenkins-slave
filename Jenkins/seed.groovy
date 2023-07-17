554a7bfaf497e706065afa935c5f7c430afe5ce5
yashwanthreddy <yashwanth.kotha@onpassive.com>
2023-07-17 11:29:39 +0530

    code changes

--- Jenkins/seed.groovy
+++ Jenkins/seed.groovy
@@ -1,25 +1,4 @@
-freeStyleJob('example') {
-    logRotator(-1, 10)
-    jdk('Java 8')
-    scm {
-        github('jenkinsci/job-dsl-plugin', 'master')
-    }
-    triggers {
-        githubPush()
-    }
-    steps {
-        gradle('clean build')
-    }
-    publishers {
-        archiveArtifacts('job-dsl-plugin/build/libs/job-dsl.hpi')
-    }
-}
-
-
-
-
-
-/*folder('CI-Pipelines') {
+folder('CI-Pipelines') {
   displayName('CI-Pipelines')
   description('CI-Pipelines')
 }
@@ -43,5 +22,5 @@ pipelineJob('CI-Pipelines/Jenkins') {
       'lightweight'(true)
     }
   }
-}/*
+}
 
