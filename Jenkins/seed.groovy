9762f7fa822415d0b41593b8cf0a23a6986ed607
yashwanthreddy <yashwanth.kotha@onpassive.com>
2023-07-17 11:09:15 +0530

    code changes

--- Jenkins/seed.groovy
+++ Jenkins/seed.groovy
@@ -3,7 +3,7 @@ folder('CI-Pipelines') {
   description('CI-Pipelines')
 }
 
-/*pipelineJob('CI-Pipelines/Jenkins') {
+pipelineJob('CI-Pipelines/Jenkins') {
   configure { flowdefinition ->
     flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
       'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
@@ -22,36 +22,5 @@ folder('CI-Pipelines') {
       'lightweight'(true)
     }
   }
-}*/
-
-
-def component = ["web1", "web2","web3"];
-
-def count=(component.size()-1)
-for (i in 0..count) {
-  def j=component[i]
-  pipelineJob("CI-Pipelines/${j}") {
-    configure { flowdefinition ->
-      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-          'userRemoteConfigs' {
-            'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/yashwanthk147/jenkins-slave.git'+j)
-              //'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
-            }
-          }
-          //'branches' {
-            //'hudson.plugins.git.BranchSpec' {
-              //'name'('*/tags/*')
-            //}
-            'hudson.plugins.git.BranchSpec' {
-              'name'('*/main')
-            }
-          }
-        }
-        'scriptPath'('Jenkinsfile')
-        'lightweight'(true)
-      }
-    }
-  }
 }
+
